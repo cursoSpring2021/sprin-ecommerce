7a737cf06f522dde299577d3e65d79e66abc4934
cursoSpring2021 <dguerra723@unab.edu.co>
2022-03-25 10:18:43 -0500

    se muestra la tabla de los productos

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -3,6 +3,7 @@ package com.curso.ecommerce.controller;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +21,9 @@ public class ProductoController {
 	private ProductoService productoService;
 
 	@GetMapping("")
-	public String show() {
+	public String show(Model model) {
+		model.addAttribute("productos",productoService.findAll());
+		
 		return "productos/show";
 	}
 
@@ -39,5 +42,7 @@ public class ProductoController {
 		return "redirect:/productos";
       
 	}
+	
+	
 
 }
