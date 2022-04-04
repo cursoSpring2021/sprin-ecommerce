577339b8591d851eb7112a4c652a0c9958bfd37a
cursoSpring2021 <dguerra723@unab.edu.co>
2022-04-04 17:51:41 -0500

    Funcionalidad para ver usuario admin

--- src/main/java/com/curso/ecommerce/service/IUsuarioService.java
+++ src/main/java/com/curso/ecommerce/service/IUsuarioService.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.curso.ecommerce.model.Usuario;
@@ -9,5 +10,6 @@ public interface IUsuarioService  {
   Optional<Usuario>	 findById(Integer id);
   Usuario save(Usuario usuario);
   Optional<Usuario> finByEmail(String email);
+  List<Usuario> findAll();
 
 }
--- src/main/java/com/curso/ecommerce/service/UsuarioServiceImp.java
+++ src/main/java/com/curso/ecommerce/service/UsuarioServiceImp.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,12 @@ public class UsuarioServiceImp implements IUsuarioService{
 	public Optional<Usuario> finByEmail(String email) {
 		
 		return usuarioRepository.findByEmail(email);
+	}
+
+	@Override
+	public List<Usuario> findAll() {
+		
+		return usuarioRepository.findAll();
 	}
 	
 
